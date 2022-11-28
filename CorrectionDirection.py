b97ddd8acd96fe160de29f14b815cc3c3c675e0a
PascalA79 <arespascal79@gmail.com>
2022-11-28 14:12:06 -0500

    CorrectionDirection V1

--- CorrectionDirection.py
+++ CorrectionDirection.py
@@ -1,18 +1,60 @@
 from Point import Point
 from Robot import Robot
+from ModuleInertiel import ModuleInertiel
+from Point import Point
 from Math import abs
+from time import sleep
+from threading import Thread
+from JetonAnnulation import JetonAnnulation
 class CorrectionDirection:
-    def __init__(self,precision=5):
-        self.__precision=precision
-    def correction_angle(self, angle_i:int, point_i:Point, point_f:Point):
+    def __init__(self,robot:Robot,gyro,gps,precision_angle=3,precision_distance=0.1):
+        self.__robot=robot
+        self.__precision_angle=precision_angle
+        self.__precision_distance=precision_distance
+        self.__gyro=gyro
+        self.__gps=gps
+        self.__destination=None
+        self.__thread=Thread(target=self.centrer)
+        self.__jeton=JetonAnnulation()
+    def __correction_angle(self, angle_i:int, point_i:Point, point_f:Point):
         return (Point.angle(point_i,point_f)-angle_i-180)%360-180
-        
-    def centrer(self,current_angle,point_ini,point_final, robot:Robot):
-        while abs(dt_angle)>self.__precision:
-            dt_angle=self.correction_angle(current_angle,point_ini,point_final)
-            if dt_angle<0:
-                robot.tourner_g()
+    
+    def set_destination(self,point:Point):
+        self.__destination=point
+
+    def demarrer(self,f_next):
+        self.__thread.start()
+
+    def centrer(self):#,current_angle,point_ini,point_final):
+        while self.__jeton.continuer():
+            current_angle=self.__gps.current_angle()
+            point_ini=self.__gps.position()
+            point_final=self.__destination
+            dt_angle=self.__correction_angle(current_angle,point_ini,point_final)
+            if abs(dt_angle)>self.__precision_angle:
+                angle_gyro_ini=self.__gyro.angle()
+
+                def obtenir_angle():
+                    return self.__gyro.angle()-angle_gyro_ini
+
+                while obtenir_angle()<dt_angle:
+                    D_angle=dt_angle+obtenir_angle()
+                    if D_angle<0:
+                        self.__robot.tourner_g()
+                    else:
+                        self.__robot.tourner_d()
             else:
-                robot.tourner_d()
+                self.__robot.avancer()
+            sleep(0.05)
+    def arreter(self):
+        self.__jeton.terminer()
+        self.__thread.join()
+def main():
+    robot=Robot.construire()
+    gyro=ModuleInertiel()
+    correcteur=CorrectionDirection(robot,gyro,None,3,0.05)
+if __name__=="__main__":
+    main()
+
             
 
