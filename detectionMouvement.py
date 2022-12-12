c6881fd45dbbea62560e0bad8c8142c983ba8394
Captain-Friendly <juliandelapaz2001@gmail.com>
2022-12-12 12:55:45 -0500

    detection mouvement

--- detectionMouvement.py
+++ detectionMouvement.py
@@ -1,16 +1,20 @@
-import time
 from Camera import Camera 
 import cv2
 import numpy as np
+from JetonAnnulation import JetonAnnulation
+
 
 def main():
+    SEUIL_MAX = 255    
+    SEUIL_MIN = 20   
+    AIRE_MIN = 500
+
     cam = Camera()
-
-    end = time.perf_counter() + 10
-
     compteur_frame = 0
     frame_precendente = None
-    while True:
+    jeton = JetonAnnulation()
+
+    while jeton.continuer():
         compteur_frame +=1
         succes, img = cam.obtenir_image()
         frame_actuelle = Camera.convert_to_grayscale(img)
@@ -31,24 +35,23 @@ def main():
             kernel = np.ones((5, 5))
             differences = cv2.dilate(differences, kernel, 1)
 
-            # prend les differences d'une taille minimum
-            differences_min =  cv2.threshold(src=differences, thresh=20, maxval=255, type=cv2.THRESH_BINARY)[1]
+            # prend les differences d'une taille minimumx
+            differences_min =  cv2.threshold(src=differences, thresh=SEUIL_MIN, maxval=SEUIL_MAX, type=cv2.THRESH_BINARY)[1]
 
 
             contours, _ = cv2.findContours(image=differences_min, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
             # cv2.drawContours(image=img, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
 
             for contour in contours:
-                if cv2.contourArea(contour) < 50:
-                    # too small: skip!
-                    continue
-                (x, y, w, h) = cv2.boundingRect(contour)
-                cv2.rectangle(img=img, pt1=(x, y), pt2=(x + w, y + h), color=(0, 255, 0), thickness=2)
+                if cv2.contourArea(contour) > AIRE_MIN:
+                    (x, y, w, h) = cv2.boundingRect(contour)
+                    cv2.rectangle(img=img, pt1=(x, y), pt2=(x + w, y + h), color=(0, 255, 0), thickness=2)
+                
 
             cv2.imshow('Motion detector', img)
 
             if cv2.waitKey(1) == ord('x'):
-                break
+                jeton.terminer()
 
 
 if __name__ == "__main__":
